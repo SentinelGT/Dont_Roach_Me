90032548b7068a810e80be8bcbabd172d168af6c
SentinelGT <blainebailey16@gmail.com>
2020-11-09 15:37:25 -0800

    UI Tutorial PopUp
    Added UI popup for player tutorial and added animations with scrolling text in the text box

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// to update the UI elements
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour
+{
+
+    public Text nameText;
+    public Text dialogueText;
+
+
+    public Animator animator;
+    private Queue<string> sentences;     //FIFO, better than a list to track sentences
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        
+    }
+
+   public void StartDialogue (Dialogue dialogue)
+   {
+       //Debug.Log("Starting conversation with " + dialogue.name);
+
+       animator.SetBool("IsOpen", true);
+
+       nameText.text = dialogue.name;
+
+       sentences.Clear();       // clears teh log
+
+       //loop through the dialogue and add them to a queu
+       foreach (string sentence in dialogue.sentences)
+       {
+           sentences.Enqueue(sentence);     //pulls the text items in teh queue
+       }
+
+        DispalyNextSentence();
+   }
+
+   public void DispalyNextSentence()
+   {
+       if (sentences.Count == 0)
+       {
+           EndDialogue();
+           return;
+       }
+
+       string sentence = sentences.Dequeue();
+
+       //statement below just prints the whole text at once, for flavor just individually
+       // dialogueText.text = sentence;
+
+        // want to check if it has stopped animating before new text
+       StopAllCoroutines();
+       StartCoroutine(TypeSentence(sentence));
+   }
+
+    //prints letters one by one
+   IEnumerator TypeSentence (string sentence)
+   {
+       dialogueText.text = "";
+       foreach (char letter in sentence.ToCharArray())
+       {
+           // throws characters from the text field into array
+           dialogueText.text += letter;
+           yield return null;
+       }
+   }
+
+   void EndDialogue()
+   {
+       Debug.Log("End Convo");
+       animator.SetBool("IsOpen", false);
+   }
+}
---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// new implementation
+using UnityEngine.Events;
+
+public class InteractableObject : MonoBehaviour
+{
+
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange)
+        {
+            if(Input.GetKeyDown(interactKey))       //check key
+            {
+                interactAction.Invoke();    //fire event
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Player in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Player out of range");
+        }
+    }
+}
