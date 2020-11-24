ac8373df5ac57773116d5ecd3ac61952142f051c
ChrisM117 <72722217+ChrisM117@users.noreply.github.com>
2020-11-24 13:07:59 -0800

    FoodStuffPickUp
    Made a FoodStuffPickUp scipt that works.

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
-   private static GameMaster instance;
+   public static GameMaster instance;
    public Vector2 lastCheckPointPos;
+
+   [Header("Foodstuffs")]
+   public int foodstuffs = 0;
 
    void Awake(){
 
@@ -16,4 +19,9 @@ public class GameMaster : MonoBehaviour
            Destroy(gameObject);
        }
    }
+
+public void IncreaseFoodstuffs(int amount) {
+    foodstuffs += amount;
+}
+
 }
